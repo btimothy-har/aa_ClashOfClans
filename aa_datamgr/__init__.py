3a215bc5ee593a6b1be08d979d05b9188b97b065
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2022-10-17 20:27:08 +0800

    Tweak __init__ py incorporate all dependencies

--- aa_datamgr/__init__.py
+++ aa_datamgr/__init__.py
@@ -5,16 +5,16 @@ import discord
 import coc
 
 from os import path
+from dotenv import load_dotenv
 
 load_dotenv()
 sys.path.append(os.getenv("RESOURCEPATH"))
-
-shutil.copy("clash_resources.py",os.getenv("RESOURCEPATH"))
 
 from redbot.core.bot import Red
 from .coc_datamgr import AriXClashDataMgr
 
 async def setup(bot:Red):
+    shutil.copy("clash_resources.py",os.getenv("RESOURCEPATH"))
     cog = AriXClashDataMgr()
     await cog.cog_initialize()
     bot.add_cog(cog)
