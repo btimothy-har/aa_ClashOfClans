c306fc3acf4383a2da91163369751ed09cb1b85e
btimothy <62061780+btimothy-har@users.noreply.github.com>
2022-12-12 09:22:07 +0800

    Close eclipse gracefuilly

--- aa_usercog/aa_usercog.py
+++ aa_usercog/aa_usercog.py
@@ -677,7 +677,7 @@ class AriXMemberCommands(commands.Cog):
 
             while session.state:
                 tries -= 1
-                if True:
+                try:
                     session.add_to_path(response)
                     if response in ['start','menu']:
                         tries = 10
@@ -736,17 +736,17 @@ class AriXMemberCommands(commands.Cog):
                     #    await ctx.send("This bit doesn't exist yet.")
                     #    response = "menu"
                 
-                # except Exception as e:
-                #     err_embed = await eclipse_embed(ctx,
-                #         message=f"**Error encountered during E.C.L.I.P.S.E. session.**"
-                #             + f"\n\nError: {e}"
-                #             + f"\n\nSession User: {session.user.mention}"
-                #             + f"\nSession ID: {session.channel.id}"
-                #             + f"\nResponse Path: {session.response_path}"
-                #             + f"\nLast Response: {response}")
+                except Exception as e:
+                    err_embed = await eclipse_embed(ctx,
+                        message=f"**Error encountered during E.C.L.I.P.S.E. session.**"
+                            + f"\n\nError: {e}"
+                            + f"\n\nSession User: {session.user.mention}"
+                            + f"\nSession ID: {session.channel.id}"
+                            + f"\nResponse Path: {session.response_path}"
+                            + f"\nLast Response: {response}")
 
-                #     await ctx.bot.send_to_owners(embed=err_embed)
-                #     response = None
+                    await ctx.bot.send_to_owners(embed=err_embed)
+                    response = None
 
                 if tries == 0 or not response:
                     session.state = False
@@ -762,11 +762,17 @@ class AriXMemberCommands(commands.Cog):
                         await session.message.clear_reactions()
                         await session.message.edit(content=ctx.author.mention,embed=session_closed)
                         await asyncio.sleep(60)
-                        await session.message.delete()
+                        try:
+                            await session.message.delete()
+                        except:
+                            pass
 
                     else:
                         if session.message:
-                            await session.message.delete()
+                            try:
+                                await session.message.delete()
+                            except:
+                                pass
                         await session.channel.send(content=ctx.author.mention,embed=session_closed,delete_after=60)
 
             try:
