f6abcc3d4e0d6f9e395319e373eebf04a3212216
btimothy <62061780+btimothy-har@users.noreply.github.com>
2023-01-02 15:07:02 +0800

    Log message fix

--- aa_datamgr/aa_datamgr.py
+++ aa_datamgr/aa_datamgr.py
@@ -698,8 +698,8 @@ class AriXClashDataMgr(commands.Cog):
                             update_season = False
 
                         log_str += f"**{clan.name} ({clan.tag})**"
-                        log_str += f"\n> Clan War: {getattr(clan.current_war,'state',None)}"
-                        log_str += f"\n> Capital Raid: {getattr(clan.current_raid_weekend,'state',None)}"
+                        log_str += f"\n> Clan War: {clan.war_state}"
+                        log_str += f"\n> Capital Raid: {clan.raid_weekend_state}"
 
                         log_str += "\n\n"
 
