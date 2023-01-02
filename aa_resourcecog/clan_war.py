10f955f4f97b6ed649eedf3ddf6e8ffba29646f5
btimothy <62061780+btimothy-har@users.noreply.github.com>
2023-01-02 10:53:42 +0800

    Debug code for CWL

--- aa_resourcecog/clan_war.py
+++ aa_resourcecog/clan_war.py
@@ -158,8 +158,9 @@ class aClanWar():
 
         elif clan.public_war_log:
             try:
-                ch = ctx.bot.get_channel(856433806142734346)
-                await ch.send(clan.tag)
+                if ctx.invoked_with == 'nstart':
+                    ch = ctx.bot.get_channel(856433806142734346)
+                    await ch.send(clan.tag)
                 war = await ctx.bot.coc_client.get_current_war(clan.tag)
             except coc.errors.PrivateWarLog:
                 return None
